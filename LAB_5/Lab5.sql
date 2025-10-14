6eb3e6e0bca0f860309fbd72682253de0ae1380a
duytrannhut <trannhutduy.xy@gmail.com>
2025-10-14 15:50:43 +0700

    Nop bai

---
+++ LAB_5/Lab5.sql
@@ -0,0 +1,281 @@
+﻿use QLDA
+-- BÀI 1: CÁC STORED PROCEDURE Cơ BẢN 
+-- 1. In ra "Xin chào + tên"
+go
+CREATE OR ALTER PROC XinChao
+    @ten NVARCHAR(50)
+AS 
+BEGIN
+	Print N'Xin chào ' + @ten
+END
+GO
+    EXEC XinChao N'Trần Hửu Hào'
+GO
+-- Câu 2: Nhập vào 2 số, in ra tổng
+CREATE OR ALTER PROC TinhTong
+    @s1 INT,
+    @s2 INT
+AS
+BEGIN
+    DECLARE @tg INT
+    SET @tg = @s1 + @s2
+    PRINT N'Tổng là: ' + CAST(@tg AS NVARCHAR(20))
+END
+GO
+
+EXEC TinhTong 10, 20
+GO
+-- Câu 3: Tổng các số chẵn từ 1 đến @n
+CREATE OR ALTER PROC TongTuN
+    @n INT
+AS 
+BEGIN
+    DECLARE @i INT = 2
+    DECLARE @tong INT = 0
+    
+    WHILE @i <= @n
+    BEGIN
+        SET @tong = @tong + @i
+        SET @i = @i + 2
+    END
+    
+    PRINT N'Tổng các số chẵn từ 1 đến ' + CAST(@n AS NVARCHAR(20)) + N' là: ' + CAST(@tong AS NVARCHAR(20))
+END
+EXEC TongTuN 10
+GO
+
+-- Câu 4: Ước chung lớn nhất của 2 số
+CREATE OR ALTER PROCEDURE sp_UCLN
+    @a INT,
+    @b INT
+AS
+BEGIN
+    DECLARE @temp INT
+    
+    -- Đảm bảo @a <= @A
+    IF @a > @A
+    BEGIN
+        SET @temp = @a
+        SET @a = @A
+        SET @b = @temp
+    END
+    
+    -- Tìm UCLN
+    WHILE @a != 0
+    BEGIN
+        SET @temp = @b % @a
+        SET @b = @a
+        SET @a = @temp
+    END
+    
+    PRINT N'Ước chung lớn nhất là: ' + CAST(@b AS NVARCHAR(20))
+END
+GO
+
+ 
+EXEC sp_UCLN 12, 18
+GO
+
+
+-- BÀI 2: THAO TÁC VỚI CSDL QLDA (3 điểm)
+-- Câu 1: Xuất thông tin nhân viên theo mã
+CREATE OR ALTER PROCEDURE sp_ThongTinNhanVien
+    @Manv INT
+AS
+BEGIN
+    SELECT * 
+    FROM NHANVIEN
+    WHERE MANV = @Manv
+END
+GO
+
+ 
+EXEC sp_ThongTinNhanVien 1
+GO
+
+-- Câu 2: Số lượng nhân viên tham gia đề án
+CREATE OR ALTER PROCEDURE sp_SoLuongNV_DeAn
+    @MaDa INT
+AS
+BEGIN
+    DECLARE @SoLuong INT
+    
+    SELECT @SoLuong = COUNT(DISTINCT MA_NVIEN)
+    FROM PHANCONG
+    WHERE MADA = @MaDa
+    
+    PRINT N'Số lượng nhân viên tham gia đề án ' + CAST(@MaDa AS NVARCHAR(10)) + N' là: ' + CAST(@SoLuong AS NVARCHAR(10))
+END
+GO
+
+ 
+EXEC sp_SoLuongNV_DeAn 1
+GO
+
+-- Câu 3: Số lượng NV theo mã đề án và địa điểm
+CREATE OR ALTER PROCEDURE sp_SoLuongNV_DeAn_DiaDiem
+    @MaDa INT,
+    @Ddiem_DA NVARCHAR(50)
+AS
+BEGIN
+    DECLARE @SoLuong INT
+    
+    SELECT @SoLuong = COUNT(DISTINCT PC.MA_NVIEN)
+    FROM PHANCONG PC
+    JOIN DEAN DA ON PC.MADA = DA.MADA
+    WHERE PC.MADA = @MaDa AND DA.DDIEM_DA = @Ddiem_DA
+    
+    PRINT N'Số lượng nhân viên: ' + CAST(@SoLuong AS NVARCHAR(10))
+END
+GO
+
+ 
+EXEC sp_SoLuongNV_DeAn_DiaDiem 1, N'TP HCM'
+GO
+
+-- Câu 4: Nhân viên không có thân nhân theo trưởng phòng
+CREATE OR ALTER PROCEDURE sp_NV_KhongThanNhan
+    @Trphg INT
+AS
+BEGIN
+    SELECT NV.*
+    FROM NHANVIEN NV
+    JOIN PHONGBAN PB ON NV.PHG = PB.MAPHG
+    WHERE PB.TRPHG = @Trphg
+    AND NV.MANV NOT IN (SELECT MANV FROM THANNHAN)
+END
+GO
+
+EXEC sp_NV_KhongThanNhan 1
+GO
+
+-- Câu 5: Kiểm tra nhân viên có thuộc phòng ban không
+CREATE OR ALTER PROCEDURE sp_KiemTraNV_PhongBan
+    @Manv INT,
+    @Mapb INT
+AS
+BEGIN
+    IF EXISTS(SELECT 1 FROM NHANVIEN WHERE MANV = @Manv AND PHG = @Mapb)
+        PRINT N'Nhân viên ' + CAST(@Manv AS NVARCHAR(10)) + N' thuộc phòng ban ' + CAST(@Mapb AS NVARCHAR(10))
+    ELSE
+        PRINT N'Nhân viên ' + CAST(@Manv AS NVARCHAR(10)) + N' KHÔNG thuộc phòng ban ' + CAST(@Mapb AS NVARCHAR(10))
+END
+GO
+
+ 
+EXEC sp_KiemTraNV_PhongBan 1, 5
+GO
+
+-- BÀI 3: INSERT, UPDATE VỚI STORED PROCEDURE (3 điểm)
+
+
+-- Câu 1: Thêm phòng ban CNTT
+CREATE OR ALTER PROC sp_ThemPhongBan
+    @Maphg NVARCHAR(9),
+    @Tenphg NVARCHAR(30),
+    @Trphg NVARCHAR(9),
+    @Ng_Nhanchuc DATE
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM PHONGBAN WHERE MAPHG = @Maphg)
+        PRINT N'Thêm thất bại: Mã phòng đã tồn tại'
+    ELSE
+    BEGIN
+        INSERT INTO PHONGBAN(MAPHG, TENPHG, TRPHG, NG_NHANCHUC)
+        VALUES(@Maphg, @Tenphg, @Trphg, @Ng_Nhanchuc)
+        PRINT N'Thêm phòng ban thành công'
+    END
+END
+GO
+
+EXEC sp_ThemPhongBan '010', N'CNTT', '005', '2024-01-01'
+GO
+
+-- Câu 2: Cập nhật phòng CNTT thành IT
+CREATE OR ALTER PROC sp_UpdatePhongCNTT
+AS
+BEGIN
+    UPDATE PHONGBAN
+    SET TENPHG = N'IT'
+    WHERE TENPHG = N'CNTT'
+    PRINT N'Đã cập nhật phòng CNTT thành IT'
+END
+GO
+
+EXEC sp_UpdatePhongCNTT
+GO
+
+USE QLDA
+GO
+
+-- 1. Thêm phòng ban CNTT (kiểm tra trùng MAPHG)
+CREATE OR ALTER PROC sp_ThemPhongBan
+    @Maphg NVARCHAR(9),
+    @Tenphg NVARCHAR(30),
+    @Trphg NVARCHAR(9),
+    @Ng_Nhanchuc DATE
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM PHONGBAN WHERE MAPHG = @Maphg)
+        PRINT N'Thêm thất bại: Mã phòng đã tồn tại'
+    ELSE
+    BEGIN
+        INSERT INTO PHONGBAN(MAPHG, TENPHG, TRPHG, NG_NHANCHUC)
+        VALUES(@Maphg, @Tenphg, @Trphg, @Ng_Nhanchuc)
+        PRINT N'Thêm phòng ban thành công'
+    END
+END
+GO
+
+-- 2. Cập nhật tên phòng CNTT -> IT
+CREATE OR ALTER PROC sp_UpdatePhongCNTT
+AS
+BEGIN
+    UPDATE PHONGBAN
+    SET TENPHG = N'IT'
+    WHERE TENPHG = N'CNTT'
+    PRINT N'Đã cập nhật phòng CNTT thành IT'
+END
+GO
+
+-- 3. Thêm nhân viên (có điều kiện)
+use QLDA;
+go
+
+CREATE OR ALTER PROC sp_ThemNhanVien
+    @Manv NVARCHAR(9),
+    @Honv NVARCHAR(15),
+    @Tenlot NVARCHAR(15),
+    @Tennv NVARCHAR(15),
+    @Phai NVARCHAR(3),
+    @NgSinh DATE,
+    @Diachi NVARCHAR(50),
+    @Luong FLOAT,
+    @Phg NVARCHAR(9)
+AS
+BEGIN
+    DECLARE @Tuoi INT = DATEDIFF(YEAR, @NgSinh, GETDATE())
+    DECLARE @MaQL NVARCHAR(9)
+
+    IF @Luong < 25000
+        SET @MaQL = '009'
+    ELSE
+        SET @MaQL = '005'
+
+    IF @Phai = N'Nam' AND (@Tuoi < 18 OR @Tuoi > 65)
+        PRINT N'Nhân viên nam không hợp lệ (tuổi 18–65)'
+    ELSE IF @Phai = N'Nữ' AND (@Tuoi < 18 OR @Tuoi > 60)
+        PRINT N'Nhân viên nữ không hợp lệ (tuổi 18–60)'
+    ELSE
+    BEGIN
+        INSERT INTO NHANVIEN(MANV, HONV, TENLOT, TENNV, PHAI, NGSINH, DCHI, LUONG, MA_NQL, PHG)
+        VALUES(@Manv, @Honv, @Tenlot, @Tennv, @Phai, @NgSinh, @Diachi, @Luong, @MaQL, @Phg)
+        PRINT N'Thêm nhân viên thành công'
+    END
+END
+GO
+
+EXEC sp_ThemNhanVien 
+    'NV999', N'Nguyễn', N'Văn', N'Hào', N'Nam', '1999-10-11',
+    N'Hà Nội', 23000, '010'
+GO
---
+++ LAB_6/Lab6.sql
@@ -0,0 +1,137 @@
+﻿USE QLDA
+GO
+--Bài 1: Viết trigger DML
+-- Trigger kiểm tra khi thêm nhân viên
+CREATE TRIGGER trg_Insert_NhanVien
+ON NhanVien
+FOR INSERT
+AS
+BEGIN
+    DECLARE @Luong INT, @Tuoi INT
+    SELECT @Luong = LUONG,
+           @Tuoi = DATEDIFF(YEAR, NGSINH, GETDATE())
+    FROM INSERTED
+
+    IF @Luong <= 15000
+    BEGIN
+        RAISERROR (N'Luong phải > 15000', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+
+    IF @Tuoi < 18 OR @Tuoi > 65
+    BEGIN
+        RAISERROR (N'Độ tuổi phải trong khoảng 18–65', 16, 1)
+        ROLLBACK TRANSACTION
+        RETURN
+    END
+END
+GO
+
+-- Trigger không cho phép cập nhật nhân viên ở TP.HCM
+CREATE TRIGGER trg_Update_NhanVien_TPHCM
+ON NhanVien
+FOR UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT * FROM INSERTED i
+        JOIN DELETED d ON i.MANV = d.MANV
+        WHERE d.DCHI LIKE N'%TP HCM%' OR d.DCHI LIKE N'%TP.HCM%'
+    )
+    BEGIN
+        RAISERROR (N'Không được cập nhật nhân viên ở TP.HCM', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+END
+GO
+
+
+--Bài 2:Trigger AFTER
+-- Sau khi thêm nhân viên -> hiển thị tổng số nam/nữ
+CREATE TRIGGER trg_AfterInsert_NhanVien
+ON NhanVien
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @SoNam INT, @SoNu INT
+    SELECT @SoNam = COUNT(*) FROM NhanVien WHERE PHAI = N'Nam'
+    SELECT @SoNu = COUNT(*) FROM NhanVien WHERE PHAI = N'Nữ'
+
+    PRINT N'Tổng số nhân viên Nam: ' + CAST(@SoNam AS NVARCHAR(10))
+    PRINT N'Tổng số nhân viên Nữ: ' + CAST(@SoNu AS NVARCHAR(10))
+END
+GO
+
+-- Sau khi cập nhật giới tính -> hiển thị tổng số nam/nữ
+CREATE TRIGGER trg_AfterUpdate_Phai
+ON NhanVien
+AFTER UPDATE
+AS
+BEGIN
+    IF UPDATE(PHAI)
+    BEGIN
+        DECLARE @SoNam INT, @SoNu INT
+        SELECT @SoNam = COUNT(*) FROM NhanVien WHERE PHAI = N'Nam'
+        SELECT @SoNu = COUNT(*) FROM NhanVien WHERE PHAI = N'Nữ'
+
+        PRINT N'Cập nhật giới tính xong!'
+        PRINT N'Tổng số nhân viên Nam: ' + CAST(@SoNam AS NVARCHAR(10))
+        PRINT N'Tổng số nhân viên Nữ: ' + CAST(@SoNu AS NVARCHAR(10))
+    END
+END
+GO
+
+-- Sau khi xóa trên bảng DEAN -> hiển thị tổng số đề án mỗi nhân viên
+CREATE TRIGGER trg_AfterDelete_DeAn
+ON DEAN
+AFTER DELETE
+AS
+BEGIN
+    PRINT N'Tổng số đề án mỗi nhân viên sau khi xóa:'
+    SELECT nv.MANV, nv.TENNV, COUNT(p.MADA) AS TongDeAn
+    FROM NhanVien nv
+    LEFT JOIN PHANCONG p ON nv.MANV = p.MA_NVIEN
+    GROUP BY nv.MANV, nv.TENNV
+END
+GO
+
+--Bài 3: Trigger INSTEAD OF
+-- Xóa nhân viên -> đồng thời xóa thân nhân liên quan
+CREATE TRIGGER trg_InsteadOfDelete_NhanVien
+ON NhanVien
+INSTEAD OF DELETE
+AS
+BEGIN
+    DELETE FROM Thannhan
+    WHERE MA_NVIEN IN (SELECT MANV FROM DELETED)
+
+    DELETE FROM NhanVien
+    WHERE MANV IN (SELECT MANV FROM DELETED)
+END
+GO
+
+-- Khi thêm nhân viên mới -> tự động phân công vào đề án MADA = 1
+CREATE OR ALTER TRIGGER trg_InsteadOfInsert_NhanVien
+ON NhanVien
+INSTEAD OF INSERT
+AS
+BEGIN
+    -- Thêm nhân viên mới đầy đủ các cột cần thiết
+    INSERT INTO NhanVien (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, MA_NQL, PHG)
+    SELECT 
+        HONV, TENLOT,TENNV,MANV,NGSINH,DCHI,PHAI,LUONG,MA_NQL,PHG
+    FROM INSERTED
+
+    -- Tự động phân công đề án MADA = 1
+    INSERT INTO PHANCONG (MA_NVIEN, MADA,STT, THOIGIAN)
+    SELECT MANV, 1, 10,3
+    FROM INSERTED
+END
+GO
+
+INSERT INTO CONGVIEC (MADA, STT, TEN_CONG_VIEC)
+VALUES (1, 3, N'Công việc mặc định cho đề án 1');
+
+INSERT INTO NhanVien (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, MA_NQL, PHG)
+VALUES (N'Nguyễn', N'Văn', N'Hào', 'NV99', '1995-01-01', N'Hà Nội', N'Nam', 20000, NULL, 1);
